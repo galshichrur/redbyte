d64c8ec620b934a4baa551eb85e9b54e746ddc1d
Gal <82903124+maybegal@users.noreply.github.com>
2025-12-25 18:34:12 +0200

    Add init db function to create tables on startup if doesnt exist

--- server/app.py
+++ server/app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from api.health import router as health_router
+from database import init_db
 
 app = FastAPI(title="RedByte Server")
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
 app.include_router(health_router)
 
 @app.get("/")
--- server/database.py
+++ server/database.py
@@ -5,3 +5,6 @@ from config import Config
 engine = create_engine(Config.DB_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
