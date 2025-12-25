1d704af80f06c822fe096df2f1db0471c557beec
Gal <82903124+maybegal@users.noreply.github.com>
2025-12-25 18:28:12 +0200

    Add endpoint model

---
+++ server/models/endpoint.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from database import Base
+
+class Endpoint(Base):
+    __tablename__ = "endpoints"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    status = Column(String, default="offline")
+    hostname = Column(String)
+    os = Column(String)
+    local_ip_address = Column(String)
+    public_ip_address = Column(String)
+    port = Column(Integer)
+
+    first_seen = Column(DateTime, server_default=func.now())
+    last_seen = Column(DateTime)
