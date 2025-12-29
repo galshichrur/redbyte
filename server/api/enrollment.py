a38143c9b164294f838a52dd2b25ba62a512211b
Gal <82903124+maybegal@users.noreply.github.com>
2025-12-29 21:27:22 +0200

    Update enrollment code to return code if already exists

--- server/api/enrollment.py
+++ server/api/enrollment.py
@@ -18,8 +18,12 @@ def create_enrollment_code(user_id: int = Depends(get_current_user_id), db = Dep
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    if get_enrollment_token(db, user.id) is not None:
-        raise HTTPException(status_code=400, detail="There is a token that already registered for this user.")
+    check_enrollment_token = get_enrollment_token(db, user.id)
+    if check_enrollment_token is not None:
+        return {
+            "token": check_enrollment_token.token,
+            "expires_at": check_enrollment_token.expires_at,
+        }
 
     token = base64.urlsafe_b64encode(urandom(8)).decode().rstrip("=")  # 64 bit
     token_hash = hashlib.sha256(token.encode()).hexdigest()
