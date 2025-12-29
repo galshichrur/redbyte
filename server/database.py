af95c6e959c07945e85bf3d576638b558100422b
Gal <82903124+maybegal@users.noreply.github.com>
2025-12-29 22:13:18 +0200

    Update database to work with Neon hosting

--- server/database.py
+++ server/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import Config
 
-engine = create_engine(Config.DB_URL, connect_args={"check_same_thread": False})
+# For sqlite database
+# engine = create_engine(Config.DB_URL, connect_args={"check_same_thread": False})
+
+engine = create_engine(Config.DB_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
