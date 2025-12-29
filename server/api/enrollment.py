03bfca4400878542d44ad57e4bbdacfc0bd81e2d
Gal <82903124+maybegal@users.noreply.github.com>
2025-12-29 20:52:32 +0200

    Add error handling and check to enrollment.py

--- server/api/enrollment.py
+++ server/api/enrollment.py
@@ -2,7 +2,7 @@ import hashlib
 import base64
 from database import get_db
 from fastapi import APIRouter, Depends, HTTPException
-from services.endpoints_services import create_enrollment_token
+from services.endpoints_services import create_enrollment_token, get_enrollment_token, check_enrollment_token_exists
 from services.validate_token import get_current_user_id
 from services.auth_services import get_user_by_userid
 from os import urandom
@@ -18,8 +18,14 @@ def create_enrollment_code(user_id: int = Depends(get_current_user_id), db = Dep
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
+    if get_enrollment_token(db, user.user_id) is not None:
+        raise HTTPException(status_code=400, detail="There is a token that already registered for this user.")
+
     token = base64.urlsafe_b64encode(urandom(8)).decode().rstrip("=")  # 64 bit
     token_hash = hashlib.sha256(token.encode()).hexdigest()
+
+    while check_enrollment_token_exists(db, token_hash) is not None:
+        token_hash = hashlib.sha256(token.encode()).hexdigest()
 
     enrollment_token = create_enrollment_token(db, user_id, token_hash)
 
