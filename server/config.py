c0aea668018f9eb67f3eb0de2714f478d5492b05
Gal <82903124+maybegal@users.noreply.github.com>
2025-12-29 17:44:08 +0200

    Add enrollment token expire time config

--- server/config.py
+++ server/config.py
@@ -14,6 +14,8 @@ class Config:
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
     JWT_EXPIRE_SECONDS = int(os.getenv("JWT_EXPIRE_SECONDS"))
 
+    # Endpoints
+    ENROLLMENT_TOKEN_EXPIRE_SECONDS = int(os.getenv("ENROLLMENT_TOKEN_EXPIRE_SECONDS"))
     # TCP Server
 
     # Database
