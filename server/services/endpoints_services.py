3955b8a242a521a7a1674dc211e39e2fa202903e
Gal <82903124+maybegal@users.noreply.github.com>
2025-12-29 20:52:44 +0200

    Add more functions to endpoints_services.py

--- server/services/endpoints_services.py
+++ server/services/endpoints_services.py
@@ -16,8 +16,23 @@ def create_enrollment_token(db, user_id: int, token_hash: str) -> EnrollmentToke
     db.refresh(enrollment_token)
     return enrollment_token
 
-#
-# def verify_token(db, token: str):
-#     token_hash = hashlib.sha256(token.encode()).hexdigest()
-#
-#     return get_valid_token(db, token_hash)
+
+def get_enrollment_token(db, user_id: int):
+    return db.query(EnrollmentToken).filter(EnrollmentToken.user_id == user_id).first()
+
+
+def check_enrollment_token_exists(db, token_hash: str):
+    return db.query(EnrollmentToken).filter(EnrollmentToken.token_hash == token_hash).first()
+
+
+def verify_enrollment_token(db, token: str):
+    token_hash = hashlib.sha256(token.encode()).hexdigest()
+
+    enrollment_token = check_enrollment_token_exists(db, token_hash)
+    if enrollment_token is not None:
+        db.remove(enrollment_token)
+        db.commit()
+        return True
+
+    return False
+
