cea2f14f874275f2c441741ea10bfe5f53e54bb5
Gal <82903124+maybegal@users.noreply.github.com>
2026-01-31 17:19:20 +0200

    Update main.cpp, refactor code, add use of new TcpClient class

--- client/backend/src/main.cpp
+++ client/backend/src/main.cpp
@@ -1,23 +1,55 @@
 ﻿#include <iostream>
-#include "nlohmann/json.hpp"
+#include <fstream>
+#include <string>
+#include "network/Client.cpp"
+#include "IPC.h"
 
-using json = nlohmann::json;
+const std::string IP = "127.0.0.1";
+const int PORT = 9000;
 
-bool is_already_enrolled() {
-    // check if user already enrolled.
+
+// Check if this computer already enrolled
+bool isAlreadyEnrolled() {
     return false;
 }
 
-bool enroll(const std::string& code) {
+// Checks if the given token is correct
+bool validateToken(TcpClient client, const std::string& code) {
+    // Send enrollment message to server
+
+    // Receive auth response from the server
     return true;
 }
 
-int main() {
-    json res;
-    res["success"] = is_already_enrolled();
+int main(int argc, char* argv[]) {
+    // Connect to the server
+    TcpClient client;
+    if (!client.connectToServer(IP, PORT)) {
+        std::cout << "Connection failed\n";
+        return 1;
+    }
 
-    std::cout << res.dump() << std::endl;
-    std::cout.flush();
+    // Check if agent token already exists on the computer
+    bool isEnrolled = isAlreadyEnrolled();
+    IPC::SendIsEnrolled(isEnrolled);
+
+    // Receive input from the frontend
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        json msg = json::parse(line);
+
+        if (msg["type"] == "submit_code") {
+            std::string code = msg["code"];
+
+            // Validate the given code
+            bool success = validateToken(client, code);
+            IPC::SendValidationResult(success);
+
+            if (success) {
+                break;
+            }
+        }
+    }
 
     return 0;
 }
