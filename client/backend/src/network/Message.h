139135fbd623a9121bdf8848cc8fab38e74dad80
Gal <82903124+maybegal@users.noreply.github.com>
2026-01-31 16:14:48 +0200

    Add new Message header file for Message struct, MessageType enum

---
+++ client/backend/src/network/Message.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+
+typedef std::vector<uint8_t> ByteArray;
+
+enum class MessageType : uint8_t {
+    ENROLL    = 0x01,
+    AUTH      = 0x02,
+    HEARTBEAT = 0x03,
+    ALERT     = 0x04,
+    COMMAND   = 0x05,
+    TERMINATE = 0x06
+};
+
+struct Message {
+    MessageType type;
+    ByteArray payload;
+};
