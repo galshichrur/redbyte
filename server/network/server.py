40b6da8472e579a362e70fe7f90fb101e934bea5
Gal <82903124+maybegal@users.noreply.github.com>
2026-02-04 17:18:41 +0200

    Add client threads set and lock

--- server/network/server.py
+++ server/network/server.py
@@ -11,6 +11,8 @@ class TCPServer:
         self.socket: socket.socket | None = None
         self.is_running: bool = False
         self.accept_thread: threading.Thread | None = None
+        self.client_threads: set[threading.Thread] = set()
+        self.lock = threading.Lock()
 
     def start(self) -> None:
         if self.is_running:
@@ -24,8 +26,8 @@ class TCPServer:
         self.is_running = True
         print(f"Server started at {self.host}:{self.port}")
 
-        # Accept new connection in separate thread
-        self.accept_thread = threading.Thread(target=, daemon=True)
+        # Accept new connections in separate thread
+        self.accept_thread = threading.Thread(target=self._accept_new_connections, daemon=True)
         self.accept_thread.start()
 
     def stop(self) -> None:
@@ -40,4 +42,22 @@ class TCPServer:
         self.socket.close()
         self.is_running = False
         self.socket = None
+
+        with self.lock:
+            for client_thread in self.client_threads:
+                client_thread.join(timeout=1)
+
         print("Server stopped.")
+
+    def _accept_new_connections(self) -> None:
+        while self.is_running:
+            client_sock, client_addr = self.socket.accept()
+            client_thread = threading.Thread(target=self._handle_client, args=(client_sock, client_addr), daemon=True)
+
+            while self.lock:
+                self.client_threads.add(client_thread)
+
+            client_thread.start()
+
+    def _handle_client(self, client_sock: socket.socket, client_addr: tuple[str, int]) -> None:
+        pass
