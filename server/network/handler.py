c34cc1814cfbb6b1f73d14e7dcef3e64016738a3
Gal <82903124+maybegal@users.noreply.github.com>
2026-02-05 22:10:59 +0200

    Add validate_agent function and use in handle_auth handler function

--- server/network/handler.py
+++ server/network/handler.py
@@ -1,30 +1,40 @@
 import os
 from database import get_db
+from models import Agent
 from protocol import MessageType, send_message
 from services.enrollment_services import verify_enrollment_code
+from services.agent_services import create_agent, validate_agent
 
-def handle_enroll(sock, payload: bytes):
+def handle_enroll(sock, payload: bytes) -> Agent | None:
     token = payload.decode("ascii")
     user = verify_enrollment_code(get_db(), token)
 
     # Send fail error ENROLL message
     if user is None:
         send_message(sock, MessageType.ENROLL, b'\x00')
-        return
+        return None
 
     # Send success ENROLL message
     agent_id = os.urandom(8)
     agent_secret = os.urandom(32)
 
+    agent = create_agent(get_db(), int(agent_id), user.id, agent_secret)
+
     # status (1 byte) | agent_id (8 bytes) | agent_secret (32 bytes)
     response = b'\x01' + agent_id + agent_secret
     send_message(sock, MessageType.ENROLL, response)
 
-    # Create new endpoint
-
+    return agent
 
 def handle_auth(sock, payload: bytes):
-    agent_id = payload[:8]
+    agent_id = int(payload[:8])
     agent_secret = payload[8:]
 
+    agent = validate_agent(get_db(), agent_id, agent_secret)
 
+    if agent is None:
+        send_message(sock, MessageType.AUTH, b'\x00')
+    else:
+        send_message(sock, MessageType.AUTH, b'\x01')
+
+    return agent
--- server/services/agent_services.py
+++ server/services/agent_services.py
@@ -3,10 +3,19 @@ from datetime import datetime
 from models.agent import Agent
 
 def create_agent(db, agent_id: int, user_id: int, agent_secret: bytes) -> Agent:
-    secret = hashlib.sha256(agent_secret).digest().decode()
+    agent_secret_hash = hashlib.sha256(agent_secret).digest().decode()
 
-    agent = Agent(id=agent_id, user_id=user_id, secret=secret, created_at=datetime.utcnow(), status=True)
+    agent = Agent(id=agent_id, user_id=user_id, secret_hash=agent_secret_hash, created_at=datetime.utcnow(), status=True)
     db.add(agent)
     db.commit()
     db.refresh(agent)
     return agent
+
+def validate_agent(db, agent_id: int, agent_secret: bytes):
+    agent_secret_hash = hashlib.sha256(agent_secret).digest().decode()
+    agent = db.query(Agent).filter(Agent.id == agent_id).first()
+    if not agent:
+        return None
+    if agent.secret_hash == agent_secret_hash:
+        return agent
+    return None
