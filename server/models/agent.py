1a0c3ff1763ed3fce4dc011595548da589360ee9
Gal <82903124+maybegal@users.noreply.github.com>
2026-02-08 17:26:41 +0200

    Update agent database model, add system info

--- server/models/agent.py
+++ server/models/agent.py
@@ -8,16 +8,29 @@ class Agent(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
+    # Credentials
     agent_id = Column(LargeBinary(8), unique=True, nullable=False)
     secret_hash = Column(LargeBinary(32), nullable=False)
     secret_created_at = Column(DateTime)
 
+    # Status
     status = Column(Boolean, default=False, nullable=False)
+
+    # Agen machine info
     hostname = Column(String)
     os = Column(String)
+    os_version = Column(String)
     local_ip_address = Column(String)
-    public_ip_address = Column(String)
-    port = Column(Integer)
+    public_ip_address = Column(String)  # from TCP connection
+    port = Column(Integer)  # from TCP connection
+    mac_address = Column(String)
 
+    # Connectivity
     first_seen = Column(DateTime, server_default=func.now())
-    last_seen = Column(DateTime)
+    connected_at = Column(DateTime)
+    disconnected_at = Column(DateTime)
+
+    # Security
+    risk_score = Column(Integer, default=0)
+    under_attack = Column(Boolean, default=False)
+
