740100d51c5712a43e2f9fd0db4fb465ad478d90
Gal <82903124+maybegal@users.noreply.github.com>
2026-03-31 22:00:27 +0300

    Remove old client C++ and Electron.js implmentation and move to only C#

--- client/frontend/build/installer.nsh
+++
@@ -1,7 +0,0 @@
-!macro customInstall
-  WriteRegStr HKCU "Software\Microsoft\Windows\CurrentVersion\Run" "RedByteAgent" '"$INSTDIR\RedByte.exe"'
-!macroend
-
-!macro customUnInstall
-  DeleteRegValue HKCU "Software\Microsoft\Windows\CurrentVersion\Run" "RedByteAgent"
-!macroend
--- client/frontend/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-  sendCode: (code) => ipcRenderer.send("submit-code", code),
-  onBackendMsg: (cb) => ipcRenderer.on("backend-msg", (_, msg) => cb(msg)),
-  sendReady: () => ipcRenderer.send("renderer-ready")
-});
