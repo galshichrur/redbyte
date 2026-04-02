8f67ce93591973da91043ddb3705d921bfbd96c7
Gal <82903124+maybegal@users.noreply.github.com>
2026-04-02 04:52:29 +0300

    Add simple Responder detection engine class

--- agent/RedByte.Agent/Detection/ResponderDetector.cs
+++ agent/RedByte.Agent/Detection/ResponderDetector.cs
@@ -1,11 +1,60 @@
+using System;
+using System.Net;
 using PacketDotNet;
+using RedByte.Agent.Network;
 
 namespace RedByte.Agent.Detection;
 
 public class ResponderDetector : IDetection
 {
+    private const ushort LlmnrPort = 5355;
+
     public void AnalyzePacket(Packet packet)
     {
-        
+        UdpPacket udp = packet.Extract<UdpPacket>();
+
+        if (udp != null && (udp.SourcePort == LlmnrPort || udp.DestinationPort == LlmnrPort))
+        {
+            string? ip = IsSuspicious(udp);
+            if (ip != null)
+            {
+                this.NotifyServer(true, ip);
+            }
+        }
+    }
+
+    private string? IsSuspicious(UdpPacket udp)
+    {
+        IPPacket? ipPacket = udp.ParentPacket as IPPacket;
+        if (ipPacket == null) return null;
+
+        if (udp.SourcePort == LlmnrPort)
+        {
+            return ipPacket.SourceAddress.ToString();
+        }
+
+        return null; 
+    }
+
+    private async void NotifyServer(bool isBlocked, string suspectedIp)
+    {
+        try 
+        {
+            AlertMessage alert = new AlertMessage(
+                "Poisoning",
+                "LLMNR",
+                2,
+                "LLMNR Spoofing detected and blocked", 
+                isBlocked, 
+                suspectedIp
+            );
+
+            TcpClientManager client = TcpClientManager.GetInstance();
+            await client.Send(alert);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
