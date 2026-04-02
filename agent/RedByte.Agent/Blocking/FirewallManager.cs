93481cf894f7e3fe9cdb7549310b31523f002ca5
Gal <82903124+maybegal@users.noreply.github.com>
2026-04-02 04:54:49 +0300

    Update firewall manager to return result if blocked

--- agent/RedByte.Agent/Blocking/FirewallManager.cs
+++ agent/RedByte.Agent/Blocking/FirewallManager.cs
@@ -1,6 +1,59 @@
+using System.Diagnostics;
+
 namespace RedByte.Agent.Blocking;
 
 public class FirewallManager
 {
-    
+    public bool BlockIpAddress(string ipAddress)
+    {
+        try
+        {
+            string ruleName = $"RedByte_Block_{ipAddress}";
+
+            string argsIn =
+                $"advfirewall firewall add rule name=\"{ruleName}\" dir=in action=block remoteip={ipAddress}";
+            ExecuteCommand(argsIn);
+
+            string argsOut =
+                $"advfirewall firewall add rule name=\"{ruleName}\" dir=out action=block remoteip={ipAddress}";
+            ExecuteCommand(argsOut);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public bool UnblockIpAddress(string ipAddress)
+    {
+        try
+        {
+            string ruleName = $"RedByte_Block_{ipAddress}";
+
+            string args = $"advfirewall firewall delete rule name=\"{ruleName}\"";
+            ExecuteCommand(args);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    private void ExecuteCommand(string arguments)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "netsh",
+            Arguments = arguments,
+            CreateNoWindow = true,
+            UseShellExecute = false
+        };
+
+        using (Process process = Process.Start(startInfo))
+        {
+            process?.WaitForExit();
+        }
+    }
 }
