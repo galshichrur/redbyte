bda1ac33955b5cd2a99979a825032c0756c74b08
Gal <82903124+maybegal@users.noreply.github.com>
2026-05-01 12:08:08 +0300

    Update startup script from registry to use scheduled task

--- agent/RedByte.Agent/SystemUtils/Startup.cs
+++ agent/RedByte.Agent/SystemUtils/Startup.cs
@@ -1,16 +1,30 @@
-﻿using Microsoft.Win32;
+﻿using System.Diagnostics;
 
 namespace RedByte.Agent.SystemUtils;
 
 public class Startup
 {
+    // Create scheduled task using schtasks.exe for startup with windows in the administrative privileges
     public static void SetStartup()
     {
-        const string RunKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
-        using (RegistryKey key = Registry.LocalMachine.OpenSubKey(RunKey, true))
+        try
         {
-            string path = Environment.ProcessPath; 
-            key.SetValue("RedByteAgent", $"\"{path}\"");
+            string path = Environment.ProcessPath;
+            
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "schtasks.exe",
+                Arguments = $"/create /tn \"RedByteAgent\" /tr \"\\\"{path}\\\"\" /sc onlogon /rl highest /f",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            
+            using Process process = Process.Start(startInfo);
+            process?.WaitForExit();
+        }
+        catch (Exception)
+        {
+            throw new Exception($"Failed to set startup task.");
         }
     }
 }
